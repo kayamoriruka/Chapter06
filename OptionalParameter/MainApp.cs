07eba96accaed4602ae06bec67c9c68082a3ba0d
kayamoriruka <kayamoriruka@hotmail.com>
2023-03-31 13:29:15 +0900

    0331

---
+++ OptionalParameter/MainApp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace OptionalParameter
+{
+    class MainApp
+    {
+        static void PrintProfile(string name, string phone = "")
+        {
+            Console.WriteLine($"Name:{name}, Phone:{phone}");
+        }
+        static void Main(string[] args)
+        {
+            PrintProfile("중근");
+            PrintProfile("관순", "000-0000-0000");
+            PrintProfile(name: "중근", phone: "000-000-0000");
+        }
+    }
+}
